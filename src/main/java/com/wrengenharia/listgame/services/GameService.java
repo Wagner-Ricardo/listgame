62aec378b2aa42f726fa48d82de777f6c0389b12
Wagner-Ricardo <wricardoeng@gmail.com>
2025-01-22 18:52:33 -0200

    GET game by id

--- src/main/java/com/wrengenharia/listgame/services/GameService.java
+++ src/main/java/com/wrengenharia/listgame/services/GameService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.wrengenharia.listgame.dto.GameDTO;
 import com.wrengenharia.listgame.dto.GameMinDTO;
 import com.wrengenharia.listgame.entities.Game;
 import com.wrengenharia.listgame.repositories.GameRepository;
+
+
 
 /*
  * Component service registration 
@@ -21,10 +25,23 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	/*
+	 * created request and use spring transactional method to salve in the database
+	 */
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto =  new GameDTO(result);
+		return dto;
+	}
 
+
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
+	
+
 }
